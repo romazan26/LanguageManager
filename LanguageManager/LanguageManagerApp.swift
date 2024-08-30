291e8afc77346857a797186d6e6541c42a5ca5d4
romazan26 <130604263+romazan26@users.noreply.github.com>
2024-08-30 08:57:20 +0300

    Initial Commit

---
+++ LanguageManager/LanguageManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanguageManagerApp.swift
+//  LanguageManager
+//
+//  Created by Роман on 30.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct LanguageManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
